c31aaefaf395e36a39ceb8901d1590836d361e46
Niestrat99 <niestrat99@gmail.com>
2023-08-09 22:13:05 +0200

    UGHHH

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ plugins {
 }
 
 repositories {
+    mavenCentral()
     mavenLocal()
     maven {
         url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
@@ -33,6 +34,12 @@ dependencies {
     compileOnly("me.clip:placeholderapi:2.11.2")
     compileOnly("org.spigotmc:spigot-api:1.20.1-R0.1-SNAPSHOT")
     compileOnly("net.ess3:EssentialsX:2.18.2")
+
+    // https://mvnrepository.com/artifact/org.jetbrains/annotations
+    implementation("org.jetbrains:annotations:24.0.1")
+
+    // https://mvnrepository.com/artifact/com.googlecode.json-simple/json-simple
+    implementation("com.googlecode.json-simple:json-simple:1.1.1")
 }
 
 group = "groupId"
